182dc668ae2dddf5fc00db56af7943ceb9125f13
aaroahdelmaa <>
2023-04-19 10:03:28 +0300

    Update laskijat.py

--- laskijat.py
+++ laskijat.py
@@ -1,63 +1,32 @@
 from typing import List, Union
 
-
+# Laskija-luokka, joka sisältää kaksi laskutoimitusta: summaa ja kerro.
+# Nämä toiminnot laskevat annettujen lukujen summan tai tulon.
 class Laskija:
-    """Luokka, joka toteuttaa eri laskutoimituksia.
-
-    Julkiset metodit:
-        summaa(Union[int, float], Union[int, float])
-        kerro(Union[int, float], Union[int, float])
-    """
-
     def summaa(self, *args: Union[int, float]) -> Union[int, float]:
-        """Palauttaa annettujen lukujen summan.
-
-        :param args: summan luvut
-        :type args: Union[int, float]
-        :return: lukujen summa
-        :rtype: Union[int, float]
-        """
+        # Palauttaa annettujen lukujen summan
         return sum(args)
 
     def kerro(self, *args: Union[int, float]) -> Union[int, float]:
-        """Palauttaa annettujen lukujen tulon.
-
-        :param args: tulon luvut
-        :type args: Union[int, float]
-        :return: lukujen tulo
-        :rtype: Union[int, float]
-        """
+        # Palauttaa annettujen lukujen tulon
         tulo = 1
         for luku in args:
             tulo *= luku
         return tulo
 
-
+# MonenLaskija-luokka, joka perii Laskija-luokan.
+# Sisältää samat laskutoimitukset kuin Laskija-luokka,
+# mutta pystyy käsittelemään mielivaltaisen määrän lukuja.
 class MonenLaskija(Laskija):
-    """Luokka, joka toteuttaa monen luvun summan ja tulon.
-
-    Perii luokan Laskija.
-
-    Julkiset metodit:
-        summaa(*Union[int, float])
-        kerro(*Union[int, float])
-    """
-
     pass
 
-
+# Funktio, joka tulostaa annetut avainsana-argumentit.
 def argumenttien_tulostaja(**kwargs):
-    """Tulostaa annetut avainsana-argumentit.
-
-    :param kwargs: tulostettavat avainsana-argumentit
-    :type kwargs: dict
-    """
     for avainsana, arvo in kwargs.items():
         print(f'Argumentin "{avainsana}" arvo on {arvo}.')
 
-
-### Seuraavat rivit tekevät tarkistustulostukset. Älä koske niihin.
-
+# Seuraavat rivit testaavat Laskija- ja MonenLaskija-luokkien toimivuutta,
+# sekä argumenttien_tulostaja-funktion toimivuutta.
 l = Laskija()
 ml = MonenLaskija()
 
